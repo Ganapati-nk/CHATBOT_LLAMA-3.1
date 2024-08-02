6e06b265393e653400a206d48c5a8d7d886df41d
Ganapati Naik <ganannaik@gmail.com>
2024-08-02 16:01:40 +0530

    Update app.py

--- app.py
+++ app.py
@@ -17,10 +17,10 @@ st.set_page_config(
 working_dir = os.path.dirname(os.path.abspath(__file__))
 load_dotenv(os.path.join(working_dir, ".env"))
 
-GROQ_API_KEY = os.getenv("GROQ_API_KEY")
+GROQ_API_KEY = st.secrets["api_keys"]["GROQ_API_KEY"]
 
 # save the api key to environment variable
-os.environ["GROQ_API_KEY"] = GROQ_API_KEY
+st.secrets["api_keys"]["GROQ_API_KEY"] = GROQ_API_KEY
 
 client = Groq()
 
